867e8f2e613b6013f9d9bc7cc5211b56fd1c3ea7
Maella Letellier <maella.letellier2@gmail.com>
2025-05-26 19:52:50 +0200

    architecture globale

---
+++ src/app/pages/events/join-event/join-event.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-event',
+  imports: [],
+  templateUrl: './join-event.component.html',
+  styleUrl: './join-event.component.scss'
+})
+export class JoinEventComponent {
+
+}
---
+++ src/app/pages/organizer/event-crud/event-crud.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-crud',
+  imports: [],
+  templateUrl: './event-crud.component.html',
+  styleUrl: './event-crud.component.scss'
+})
+export class EventCrudComponent {
+
+}
